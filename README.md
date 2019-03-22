accb129207ca52edb21675571d9e04ba95652a2c
Ian P Bradley <ian.bradley@studiocrabapple.com>
2019-03-22 08:56:21 -0700

    First commit

---
+++ README.md
@@ -0,0 +1,26 @@
+# NAME
+
+Google::Maps::Client - Blah blah blah
+
+# SYNOPSIS
+
+    use Google::Maps::Client;
+
+# DESCRIPTION
+
+Google::Maps::Client is
+
+# AUTHOR
+
+Ian P Bradley <ian.bradley@studiocrabapple.com>
+
+# COPYRIGHT
+
+Copyright 2019- Ian P Bradley
+
+# LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# SEE ALSO
---
+++ lib/Google/Maps/Client.pm
@@ -0,0 +1,39 @@
+package Google::Maps::Client;
+
+use strict;
+use 5.008_005;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Google::Maps::Client - Blah blah blah
+
+=head1 SYNOPSIS
+
+  use Google::Maps::Client;
+
+=head1 DESCRIPTION
+
+Google::Maps::Client is
+
+=head1 AUTHOR
+
+Ian P Bradley E<lt>ian.bradley@studiocrabapple.comE<gt>
+
+=head1 COPYRIGHT
+
+Copyright 2019- Ian P Bradley
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 SEE ALSO
+
+=cut
